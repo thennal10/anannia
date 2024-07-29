8080d646a852c3d4ee4f94d15e119a22b46ca547
thennal10 <thennal10@gmail.com>
2024-07-29 20:39:20 +0530

    made it handle jpeg files properly

--- convert_to_webp.py
+++ convert_to_webp.py
@@ -8,7 +8,8 @@ files = list(os.scandir(sys.argv[1]))
 filenames = [f.name for f in files]
 
 for file in files:
-    if file.name.endswith(('jpg', 'png')):
-        if f"{file.name[:-3]}webp" not in filenames:
-            subprocess.run(['cwebp', '-q', '80', file.path, '-o', f'{file.path[:-3]}webp'])
+    if file.name.endswith(('jpg', 'png', 'jpeg')):
+        def remove_extension(filename): return '.'.join(filename.split('.')[:-1]) 
+        if f"{remove_extension(file.name)}.webp" not in filenames:
+            subprocess.run(['cwebp', '-q', '80', file.path, '-o', f'{remove_extension(file.path)}.webp'])
 
